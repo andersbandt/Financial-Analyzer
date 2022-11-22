b492832818dae68cea95e66acd600332fee3bc56
Anders <andersbandt@yahoo.com>
2022-11-22 17:45:05 -0600

    testing pushes from Anders-LAPTOP

--- categories/Category.py
+++ categories/Category.py
@@ -1,28 +1,28 @@
-# import needed packages
-# import gobject
-import os
-import xml.dom.minidom
-from xml.sax.saxutils import escape
 
-"""
-Categories represents the possible financial categories set in categories.xml and sub_categories.xml
-categories.xml contains all the master categories
-sub_categories.xml contains all the children categories
-"""
+# import needed modules
+from tkinter import *
+
+# import user defined packages
+from categories import category_helper
 
 
+# TODO: refactor 'category_id' to 'id'. Might be tedious
 class Category:
     def __init__(self, category_id, name, parent, keywords):
         self.category_id = int(category_id)
         self.name = name
         self.parent = parent
 
+        self.children = category_helper.get_category_children(category_id)  # uncomment when function is finished
+
         self.keyword = []
         self.description = None
 
+        # populate keywords
         for keyword in keywords:
             if self.category_id == keyword[1]:
                 self.keyword.append(keyword[2])
+
 
     # print_category: prints a categories name to the console
     def print_category(self):
@@ -35,3 +35,20 @@ class Category:
     # getName: gets the name of a category
     def getName(self):
         return self.name
+
+
+    def draw_Category_gui_obj(self, canvas, x, y):
+        # This will create style object
+        style = Style()
+
+        # This will be adding style, and
+        # naming that style variable as
+        # W.Tbutton (TButton is used for ttk.Button).
+        style.configure('W.TButton', font=('calibri', 10, 'bold', 'underline'), foreground='red')
+
+        btn1 = Button(canvas, text=self.name, style='W.TButton', command=self.inter_gui)
+        btn1.grid(row=y, column=x, padx=10)
+
+
+    def inter_gui(self):
+        print("Executing interactive category function")
