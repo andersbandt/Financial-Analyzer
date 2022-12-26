ea3d15ae70d2be3e0a1159cc14063e6528fe8a19
Yazid <yazeedkurdi@hotmail.com>
2022-12-26 16:56:03 +0300

    Initiliaze main file

---
+++ main/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+from main.users.routes import users
+
+app.register_blueprint(users)
+
+with app.app_context():
+    # Create tables in the database
+    db.create_all()
