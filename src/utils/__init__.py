e70f93832d7e4526b6c0c9495996ef1ac975f081
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2023-01-02 13:37:59 -0500

    add logging to analysis module

---
+++ src/utils/__init__.py
@@ -0,0 +1,16 @@
+from loguru import logger
+
+
+def logfn(fn):
+    from functools import wraps
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        logger.info(f"About to run {fn.__name__} with args {locals()}")
+
+        out = fn(*args, **kwargs)
+
+        logger.info("Done running {fn.__name__} with return value {out}")
+        return out
+
+    return wrapper
